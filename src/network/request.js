0b4ea7c24dd664239e07ecb7eb0cd09a7647265b
ZTCoder <55568862+ZTCoder@users.noreply.github.com>
2020-07-18 08:45:52 +0800

    修改网络接口

--- src/network/request.js
+++ src/network/request.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export function request(config) {
   //创建实例
   const instance = axios.create({
+    //具体接口需要添加why老师微信
     baseURL: 'http://123.207.32.32:8000',
     timeout: 5000
   });
